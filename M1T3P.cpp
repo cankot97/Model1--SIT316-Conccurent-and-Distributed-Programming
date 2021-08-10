39599b070b00b8aa04d7ef8062ba59c8de840bdf
cankot97 <63225267+cankot97@users.noreply.github.com>
2021-08-10 22:21:52 +1000

    Add files via upload

--- M1T3P.cpp
+++ M1T3P.cpp
@@ -1,42 +1,29 @@
-const int buttonPin = 2;     
-const int ledPin =  13;      
-int celsius = 0;
-
-// variables will change:
-volatile int buttonState = 0;         
+int INTPIN1 = 2; 
+int INTPIN2 = 3;
+int ledPin = 13;
 void setup() {
-  
-  pinMode(A0, INPUT);
+  // put your setup code here, to run once: 
   Serial.begin(9600);
-  // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  // Attach an interrupt to the ISR vector
-  attachInterrupt(digitalPinToInterrupt(buttonPin), pin_ISR, CHANGE);
+  pinMode(INTPIN1, INPUT_PULLUP);
+  pinMode(INTPIN2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTPIN1), ISR1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(INTPIN2), ISR2, CHANGE);
+}
+
+void ISR1(){
+  Serial.print("Motion sensor 1 :");
+  Serial.println(digitalRead(INTPIN1));
+  digitalWrite(ledPin, HIGH);
+}
+
+void ISR2(){
+  Serial.print("Motion sensor 2:");
+  Serial.println(digitalRead(INTPIN2));
+  digitalWrite(ledPin, HIGH);
+	
 }
 
 void loop() {
-  
-  celsius = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
-  Serial.print(celsius);
-  Serial.println("C");
-  if(celsius >= 40)
-  {
-    digitalWrite(ledPin,HIGH);
-  	Serial.print("Too Hot");
-  	Serial.println("Temperature: LED ON");
-  }
-  else
-  {
-      digitalWrite(ledPin,LOW);
 
-  }
-  delay(1000); 
-}
-
-void pin_ISR() {
-  buttonState = digitalRead(buttonPin);
-  digitalWrite(ledPin, buttonState);
-  Serial.println("INTERRUPTED");
 }
